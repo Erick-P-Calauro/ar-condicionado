a68253847390403fea87dae0b086115b88a6dac9
Erick-P-Calauro <erickcalauro@gmail.com>
2024-10-26 10:57:44 -0400

    Protótipo versão FECINTEC

--- configurando_rede.ino
+++ configurando_rede.ino
@@ -9,10 +9,13 @@
 AsyncWebServer server(80);
 HTTPClient http;
 
-static String wifiSSID;
-static String wifiPassword;
-static String urlServer = "http://192.168.1.31:8080/ping";
-static String availableNetworks = "";
+const uint16_t kIrLed = 4;
+IRsend irsend(kIrLed);
+
+static String wifiSSID = "JOCA&ROSE 2.4G";
+static String wifiPassword = "Hsc0129Epc&";
+static String urlServer = "http://192.168.0.159:8080/ping";
+//static String availableNetworks = "";
 
 // 0 => dirError => Restart LittleFS
 // 1 => connectWifi => run tryConnectWifi and read wifiList
@@ -22,33 +25,20 @@ static int state;
 
 void setup() { 
   Serial.begin(115200);
-  wifiScan();
+  irsend.begin();
   
-  Serial.print("\n[Wifi] Scanning complete...");
-  // state => dirError/connectWifi
-  initDir();
-  Serial.print("\n[DIR] => Initialized");
-  // state => ping/initAP;
   tryConnectWifi();
   Serial.print("\n[WiFi] => Initialized");
-  // state => connectWifi/nothing 
+  
   initServer();    
   Serial.print("\n[Server] => Initialized");
 }
 
 void loop() {
   switch(state) {
-    case 0:
-      Serial.print("\n[State] => 0");
-      initDir();
-      break;
     case 1:
       Serial.print("\n[State] => 1");
       tryConnectWifi();
-      break;
-    case 2:
-      Serial.print("\n[State] => 2");
-      initAP();
       break;
     case 3:
       Serial.print("\n[State] => 3");
--- tryConnectWifi.ino
+++ tryConnectWifi.ino
@@ -1,70 +1,25 @@
 // WiFi connection logic
 void tryConnectWifi(){
   WiFi.mode(WIFI_STA);
-  File file = LittleFS.open("/root/wifiList.txt", "r");
-  if(!file || file.isDirectory() || !file.available()){
-      Serial.println("\n[File] => failed to open wifiList for reading.");
-      state = 2;
-      return;
+  
+  // Trying to connect
+  WiFi.begin(wifiSSID, wifiPassword);
+  Serial.print("[WiFi] Trying to connect on ");
+  Serial.print(wifiSSID+"\n");
+
+  // 10 Seconds to connect
+  for(int x = 0; x < 1000; x++){
+    Serial.print(".");
+    delay(10);
   }
-
-  // WiFi logic main loop
-  while(WiFi.status() != WL_CONNECTED) {
-    String wSSID;
-    String wPassword;
+  Serial.print("\n");
     
-    // 0 => SSID - 1 => Password - 2 => ',';
-    int localState = 0;
-    
-    Serial.println("[WiFi] => Entering wifi list reading...");
-    while(file.available()){
-      char content = file.read();
-
-      // End of file
-      if(content == EOF) {
-       state = 2;
-       return;
-      }
-      
-      // End of SSID
-      if(content == ',') {
-        localState = 2;
-      }
-
-      // End of line
-      if(content == '\n') {
-        break;
-      }
-
-      // Appending content 
-      if(localState == 0) {
-        wSSID += content;
-      }else if(localState == 1) {
-        wPassword += content; 
-      }else {
-        localState = 1;
-      }
-    }
-
-    // Trying to connect
-    WiFi.begin(wSSID, wPassword);
-    Serial.print("[WiFi] Trying to connect on ");
-    Serial.print(wSSID+"\n");
-
-    // 10 Seconds to connect
-    for(int x = 0; x < 1000; x++){
-      Serial.print(".");
-      delay(10);
-    }
-    Serial.print("\n");
-    
-    if(WiFi.status() != WL_CONNECTED) {
-      Serial.print("Connection failed");
-    }else {
-      state = 3;
-    }
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connection failed");
+    state = 1;
+  }else {
+    state = 3;
   }
-  file.close();
 
   Serial.print("\n[WiFi] Connected on ");
   Serial.print(WiFi.SSID()+" ");
